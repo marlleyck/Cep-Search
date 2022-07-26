ee90b58701313acca6adb51d5a9f9773390b44b0
Marlleyck Nathan <marlleyck@hotmail.com>
2022-07-26 19:55:47 -0300

    created function search cep

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,49 @@
 import './App.css';
+import { FiSearch } from 'react-icons/fi'
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+
+  const url = 'https://viacep.com.br/ws/'
+
+  const [cep, setCep] = useState({})
+
+  const [input, setInput] = useState('')
+
+  const searchCep = async () => {
+
+    if (cep === '') {
+      alert('Digite algum CEP!')
+      return
+    }
+
+    /* try {
+      const {data} = await axios.get(`${url}/${input}/json`)
+      setInput('')
+      setCep(data)
+    }
+    catch {
+      alert('Ops! Não foi possível buscar o cep!')
+      setInput('')
+    } */
+
+    const {data} = axios.get(`${url}/${input}/json`)
+    .then(({data}) => {
+      setInput('')
+      setCep(data)
+    })
+    .catch(err => {
+      if (err || Object.keys(cep) == 1) {
+        alert('Ops! Não foi possível buscar seu CEP!')
+        setInput('')
+      }
+    })
+
+
+
+  }
+
   return (
     <div className="App">
 
@@ -12,16 +55,25 @@ function App() {
 
       <main className='main'>
 
-        <label htmlFor="cep">CEP: </label>
-        <input type="text" id='cep' />
+        <fieldset className='fieldset'>
+          <label htmlFor="cep">CEP: </label>
+          <input type="text" id='cep'
+          value={input}
+          onChange={(e) => setInput(e.target.value)} />
+          <button onClick={searchCep}>
+            <FiSearch size='25px' color='#000' />
+          </button>
+        </fieldset>
 
-        <div className="details">
-          <span>Caraúbas - RN</span>
-          <span>Bairro: Centro</span>
-          <span>Rua: Av Coronel Rosendo Fernandes</span>
-          <span>Complemento: Perto casa de cultura</span>
-          <span>DDD: 84</span>
-        </div>
+        {Object.keys(cep).length > 1 && (
+          <div className="details">
+            <span>{cep.localidade} - {cep.uf}</span>
+            <span>Bairro: {cep.bairro}</span>
+            <span>Rua: {cep.logradouro}</span>
+            <span>Complemento: {cep.complemento}</span>
+            <span>DDD: {cep.ddd}</span>
+          </div>
+        )}
       </main>
     </div>
   );
